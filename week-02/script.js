18128083eaf33eff5f96350b4d6a040183f0b81c
Rexelian <leealexander061@gmail.com>
2026-01-30 13:05:04 -0500

    week 2

---
+++ week-02/script.js
@@ -0,0 +1,25 @@
+
+
+console.log("Hello, this is week " + week + ".");
+
+//alert("hello, this is really week 2")
+
+var today = "Thursday";
+var tomorrow = "Friday";
+
+//console.log("Today is Thursday");
+//console.log("Tomorrow is Friday");
+
+var greeting = "Today is " + today + ".";
+
+console.log(greeting);
+
+var week = 2;
+var nextWeek = week + 1;
+
+var weekNext = nextWeek.toString()
+
+console.log("Next week will be week " + weekNext);
+
+var myName = prompt("what is your name?");
+alert("My name is " +myName);
