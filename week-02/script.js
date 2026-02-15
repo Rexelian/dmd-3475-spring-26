ba98b7cfaf326228ed78f3287932093875231708
Rexelian <leealexander061@gmail.com>
2026-02-15 17:11:30 -0500

    updates to repo

--- week-02/script.js
+++ week-02/script.js
@@ -1,5 +1,5 @@
 
-
+var week = 2;
 console.log("Hello, this is week " + week + ".");
 
 //alert("hello, this is really week 2")
@@ -14,12 +14,11 @@ var greeting = "Today is " + today + ".";
 
 console.log(greeting);
 
-var week = 2;
+
 var nextWeek = week + 1;
-
-var weekNext = nextWeek.toString()
-
 console.log("Next week will be week " + weekNext);
+
+var weekNext = nextWeek.toString();
 
 var myName = prompt("what is your name?");
 alert("My name is " +myName);
